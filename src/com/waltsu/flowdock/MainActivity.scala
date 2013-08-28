8287a95673e493d66bfa5814d00089f8313388e6
Valtteri Virtanen <valtteri.virtanen@ecom.fi>
2013-08-29 00:17:34 +0300

    Add loading menu item to main activity

--- src/com/waltsu/flowdock/MainActivity.scala
+++ src/com/waltsu/flowdock/MainActivity.scala
@@ -12,10 +12,14 @@ import android.widget.AdapterView
 import android.view.View
 import android.widget.Adapter
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent._
 import com.waltsu.flowdock.models.Flow
 import android.content.Intent
+import android.view.MenuItem
 
 class MainActivity extends Activity {
+	var menuProgress: Option[MenuItem] = None
+
 	override def onCreate(savedInstaneState: Bundle): Unit = {
 	  super.onCreate(savedInstaneState)
 	  setContentView(R.layout.activity_main)
@@ -32,23 +36,43 @@ class MainActivity extends Activity {
 	}
 	
 	override def onCreateOptionsMenu(menu: Menu): Boolean = {
-	  getMenuInflater().inflate(R.menu.main, menu)
+	  getMenuInflater().inflate(R.menu.loading_menu, menu)
+	  val menuItem = menu.findItem(R.id.menuItemProgress).asInstanceOf[MenuItem]
+	  menuProgress = menuItem match {
+	    case null => None
+	    case m => Some(m)
+	  }
 	  true
 	}
 	
 	override def onResume(): Unit = {
 	  super.onResume();
-
-	  FlowdockApi.getFlows() onSuccess {
+	  val flowPromise = FlowdockApi.getFlows()
+	  val usersPromise = FlowdockApi.getUsers()
+	  
+	  flowPromise onSuccess {
 	    case flows =>
           utils.runOnUiThread(this, () => flowList.setAdapter(flowListAdapter(flows)))
 	  }
-	  FlowdockApi.getUsers onSuccess {
-	    case users => ApplicationState.currentUsers = users
-	  }
+	  usersPromise onSuccess {
+	    case users => 
+	      ApplicationState.currentUsers = users
+	  } 
+	  
+	  for {
+	    flowDone <- flowPromise
+	    usersDone <- usersPromise
+	  } yield toggleLoading(false)
 	}
 	
 	def flowList: ListView = findViewById(R.id.flowList).asInstanceOf[ListView]
+	def toggleLoading(visible: Boolean) = {
+	 menuProgress match {
+	   case Some(item) => utils.runOnUiThread(MainActivity.this, () => item.setVisible(visible))
+	   case None => Log.v("debug", "No menu item available")
+	   
+	 }
+	}
 	
 	def flowListAdapter(flows: List[Flow]): ArrayAdapter[Flow] = {
 	  new FlowAdapter(getApplicationContext(), flows)
